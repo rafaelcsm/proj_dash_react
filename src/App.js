7e99a91b80c06911229880486b4140c9b6e8b196
Rafael Martini <rafaelcsmartini@gmail.com>
2022-05-26 00:35:04 -0300

    Versão inicial

---
+++ src/App.js
@@ -0,0 +1,34 @@
+//import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+  </header>*/}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={"Dashboard"} />
+          <Route path="/faleconosco" element={"Entre em contato com a gente"} />
+          <Route path="/quemsomos" element={"Somos uma empresa"} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
